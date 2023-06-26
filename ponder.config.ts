c6c6dbd539488c861f2f2133e58e93725dec7ab9
0xhitgo <0xhitgo@sentiment.xyz>
2023-06-26 17:35:28 +0530

    feat: adds event handlers for deposit/claim/withdraw
    Signed-off-by: 0xhitgo <0xhitgo@sentiment.xyz>

--- ponder.config.ts
+++ ponder.config.ts
@@ -12,5 +12,12 @@ export const config: PonderConfig = {
       address: "0xb271d2c9e693dde033d97f8a3c9911781329e4ca",
       startBlock: 14743750,
     },
+    // {
+    //   name: "JPEGAuction",
+    //   network: "mainnet",
+    //   abi: "./abis/JPEGAuction.json",
+    //   address: "0x0bb49d6d3c51708cb0c810e169455022f5282630",
+    //   startBlock: 14884766,
+    // },
   ],
 };
--- src/LPFarming.ts
+++ src/LPFarming.ts
@@ -1,9 +1,50 @@
 import { ponder } from "@/generated";
+import { randomUUID } from "crypto";
 
-ponder.on("LPFarming:Claim", async ({ event, context }) => {
-  console.log(event.params);
+ponder.on("LPFarming:Withdraw", async ({ event, context }) => {
+  // console.log(event.block.timestamp);
+
+  const { Withdraw } = context.entities;
+
+  const withdraw = await Withdraw.create({
+    id: event.transaction.hash.concat(String(event.log.logIndex)),
+    data: {
+      amount: event.params.amount,
+      user: event.params.user,
+      pid: event.params.pid,
+      timestamp: event.block.timestamp,
+    },
+  });
 });
 
-ponder.on("LPFarming:ClaimAll", async ({ event, context }) => {
-  console.log(event.params);
+ponder.on("LPFarming:Claim", async ({ event, context }) => {
+  // console.log(event.block.timestamp);
+
+  const { Claim } = context.entities;
+
+  const claim = await Claim.create({
+    id: event.transaction.hash.concat(
+      String(event.log.logIndex).concat(randomUUID())
+    ),
+    data: {
+      amount: event.params.amount,
+      user: event.params.user,
+      pid: event.params.pid,
+      timestamp: event.block.timestamp,
+    },
+  });
+});
+
+ponder.on("LPFarming:Deposit", async ({ event, context }) => {
+  const { Deposit } = context.entities;
+
+  const deposit = await Deposit.create({
+    id: event.transaction.hash.concat(String(event.log.logIndex)),
+    data: {
+      amount: event.params.amount,
+      user: event.params.user,
+      pid: event.params.pid,
+      timestamp: event.block.timestamp,
+    },
+  });
 });
